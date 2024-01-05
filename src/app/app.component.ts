cdbf26a419ac081c3f6c4e936b7b7fb5f5875ce5
avto <tandila.92@gmail.com>
2024-01-06 01:45:23 +0400

    1st

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserDashboardComponent } from './UserComponents/user-dashboard/user-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'online-show-syll';
 }
+
+
